71390492ed88dd1403cb540d0de43f7a8ef62c8b
CodeHedgehog <codehedgehog@outlook.com>
2018-07-25 20:27:37 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+# ASP.NET Core Test
+
+
+### References
+
+* [Youtube: Hosting ASP.NET Core 2.1 in IIS (ANCM, in-process model)](https://www.youtube.com/watch?v=5uSWXwkFPSA)
+* [2018-02-28 ASP.NET Core 2.1.0-preview1: Improvements to IIS hosting](https://blogs.msdn.microsoft.com/webdev/2018/02/28/asp-net-core-2-1-0-preview1-improvements-to-iis-hosting/)
+  * [Notes from changing AspNet Core 2.1-preview1 app to IIS In-Process hosting](https://berserkerdotnet.github.io/blog/aspnet-core-2-1-inprocess-hosting/)
+
+* [Configuration of IIS with web.config](https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/iis/?view=aspnetcore-2.1&tabs=aspnetcore2x#configuration-of-iis-with-webconfig)
+---
+* [NuGet package: Microsoft.AspNetCore.Server.IIS](https://www.nuget.org/packages/Microsoft.AspNetCore.Server.IIS)
+* [GitHub IIS Integration](https://github.com/aspnet/IISIntegration)
+  * [GitHub IISIntegration issue #562 merged:  Add IServerVariablesFeature and enable fetching variables directly](https://github.com/aspnet/IISIntegration/pull/562)
+
+---
+* [More on ASP.NET Core Running under IIS](https://weblog.west-wind.com/posts/2017/Mar/16/More-on-ASPNET-Core-Running-under-IIS)
+* [2018-02-27: Migrating an ASP.NET Core 2.0.x project to 2.1.0-preview1](https://blogs.msdn.microsoft.com/webdev/2018/02/27/asp-net-core-2-1-0-preview1-now-available/)
---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace AspNetCoreTest
+{
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.AspNetCore.Http;
+	using Microsoft.Extensions.DependencyInjection;
+	using System.Diagnostics;
+	//using Microsoft.AspNetCore.Server.IIS;
+
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Hello World!\n\n");
+				await context.Response.WriteAsync($"Hello World from {Process.GetCurrentProcess().ProcessName}\n\n");
+				// accessing IIS server variables
+				//await context.Response.WriteAsync("Server Variables:" + Environment.NewLine);
+				//foreach (var varName in IISServerVarNames)
+				//{
+				//	await context.Response.WriteAsync(varName + ": " + context.GetIISServerVariable(varName) + Environment.NewLine);
+				//}
+			});
+		}
+	}
+}
